79e7207d4e0bd8e22867f24f435370df96311b41
leo1200213 <leo1200213870912@gmail.com>
2023-04-08 17:37:45 +0800

    CSRf front

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,5 +1,21 @@
-import { user } from "./user";
+import { user } from "./user.js";
+import { auth } from "./auth.js";
+import api from "./axiosClient";
+
 const services = {
+    auth,
 user,
 };
+api.interceptors.request.use(
+  async (config) => {
+    const { csrfToken } = await auth.getCsrf();
+    config.headers["x-csrf-token"] = csrfToken;
+    return config;
+  },
+  null,
+  {
+    runWhen: (config) =>
+      ["post", "put", "patch", "delete"].includes(config.method),
+  }
+);
 export default services;
--- frontend/src/services/user.js
+++ frontend/src/services/user.js
@@ -4,4 +4,8 @@ async getAll() {
 const { data } = await api.get("/users");
 return data;
 },
+async createOne({ name }) {
+    const { data } = await api.post("/users", { name });
+    return data;
+  },
 };
