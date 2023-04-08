afe0e13be395da730b63cbdb91df1debee342470
leo1200213 <leo1200213870912@gmail.com>
2023-04-08 17:39:25 +0800

    CSRf front2

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,14 @@
-import express from "express";
-import users from "./api/v1/users/index.js";
-const rootRouter = express.Router();
-rootRouter.use("/api/v1/users", users);
-export default rootRouter;
+//import express from "express";
+//import users from "./api/v1/users/index.js";
+//const rootRouter = express.Router();
+//rootRouter.use("/api/v1/users", users);
+//export default rootRouter;
+
+import { Router } from "express";
+import { getCsrfToken } from "./handlers.js";
+
+const router = Router();
+router.get(`/csrf-token`, getCsrfToken);
+export default router;
 
 
