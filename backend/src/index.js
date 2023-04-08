905cff714de0515cabeea3560998759f5bbefaa1
leo1200213 <leo1200213870912@gmail.com>
2023-04-08 15:34:37 +0800

    Add database connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import path, { dirname } from "path";
+import { prisma } from "./adapters.js";
+import rootRouter from "./routes/index.js";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const frontendDir = path.join(__dirname, "../../frontend/dist");
@@ -7,12 +9,11 @@ const frontendDir = path.join(__dirname, "../../frontend/dist");
 const port = process.env.PORT || 8000;
 const app = express();
 
-import rootRouter from "./routes/index.js";
 app.use(express.static(frontendDir));
 
 app.use(rootRouter); 
 
-app.get("*", (req, res) => { // Keep as the last route
+app.get("*", (req, res) => { // Keep as the last routeç
 if (!req.originalUrl.startsWith("/api")) {
 return res.sendFile(path.join(frontendDir, "index.html"));
 }
@@ -21,3 +22,8 @@ return res.status(404).send();
 app.listen(port, () => {
 console.log(`Example app listening at http://localhost:${port}`);
 });
+
+process.on("exit", async () => {
+    await prisma.$disconnect();
+    });
+    
