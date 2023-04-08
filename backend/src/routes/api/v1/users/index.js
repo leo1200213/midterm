bcd35acfadc7adea88b5f16feadb8b9cd592e6f6
leo1200213 <leo1200213870912@gmail.com>
2023-04-08 15:44:31 +0800

    Add more user api

--- backend/src/routes/api/v1/users/index.js
+++ backend/src/routes/api/v1/users/index.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
-import { getAllUsers } from "./handlers.js";
+import { createOneUser, getAllUsers, getOneUser } from "./handlers.js";
 const router = Router();
 router.get(`/`, getAllUsers);
+router.post(`/`, createOneUser); 
+router.get(`/:id`, getOneUser);
 export default router;
