f5571a285824ac1ff770bed10633c15db10d68c6
leo1200213 <leo1200213870912@gmail.com>
2023-04-19 15:44:10 +0800

    ADD about image

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -1,5 +1,21 @@
+import image from '../../0-6.jpg'
+import "./small.css"
 export default function About() 
 {
-    return <h1>Hi. I'm new to the world of frontend and backend web design. </h1>;
+
+    return (
+
+        <div classname = "pic">
+            <img
+              className="mx-auto h-60 w-auto"
+              src={image}
+              alt="You"
+            />
+        <h3 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+        Hi. I'm new to the world of frontend and backend web design.
+        </h3>
+      </div>
+    
+    );
 }
   
